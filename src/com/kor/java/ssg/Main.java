fb9dc5e7c35aaa197c4c4c5f4f49d821fe07364a
GeonhuiBaek <bkh3441@gmail.com>
2022-05-17 20:24:58 +0900

    게시물 작성 기능 구현중

--- src/com/kor/java/ssg/Main.java
+++ src/com/kor/java/ssg/Main.java
@@ -2,6 +2,8 @@ package com.kor.java.ssg;
 
 import java.util.Scanner;
 
+import sun.java2d.opengl.WGLSurfaceData.WGLVSyncOffScreenSurfaceData;
+
 
 
 public class Main {
@@ -9,15 +11,41 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("명령어를 입력해주세요");
 		
-		String command = sc.nextLine();
 		
-		System.out.println("입력된 명령어 : "+command);
-		
-		command = sc.nextLine();
-		
-		System.out.println("입력된 명령어 : "+command);
+		while (true) {
+			System.out.println("명령어) ");
+			String command = sc.nextLine();
+			int lastArticleid =0;
+			
+			command = command.trim();
+			
+			if (command.length()== 0) {
+				continue;
+			}
+			
+			if (command.equals("System exit")){
+				break;
+			}
+			else if(command.equals("article write")) {
+				int id = lastArticleid+1;
+				lastArticleid = id;
+				System.out.printf("제목 :");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%d번째 글이 작성되었습니다.\n", lastArticleid);
+				
+			}
+			else if(command.equals("article list")) {
+				System.out.println("게시물이 없습니다.");
+				
+			}
+			else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어입니다.\n", command);
+			}
+		}
 		
 		
 		sc.close();
