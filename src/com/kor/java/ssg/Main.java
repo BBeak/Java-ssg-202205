288a559d7dfe45a66d2db4a40f67bf9bcd85fb52
GeonhuiBaek <bkh3441@gmail.com>
2022-05-18 18:58:15 +0900

    게시판 검색 기능 추가중

--- src/com/kor/java/ssg/Main.java
+++ src/com/kor/java/ssg/Main.java
@@ -9,7 +9,6 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
 		int lastArticleid = 0;
-		List<Integer> lastIds = new ArrayList<>();
 		List<article> list = new ArrayList<article>();
 		String title;
 		String body;
@@ -37,15 +36,27 @@ public class Main {
 
 				System.out.printf("%d번째 글이 작성되었습니다.\n", id);
 
-			} else if (command.equals("article list") && lastArticleid != 0) {
-				System.out.printf(" 현재 %d개의 글이 있습니다.\n", lastArticleid);
-				System.out.printf("	번호	|	제목	|	내용\n");
-				for (int i = 0; i < list.size(); i++) {
-					System.out.printf("	%d	|	%s	|	%s\n", list.get(i).getid(), list.get(i).getTitle(), list.get(i).getbody());
+			} else if (command.equals("article list")) {
+				if (list.size() == 0) {
+					System.out.println("작성된 게시물이 없습니다.");
 				}
-			} else if (command.equals("article list") && lastArticleid == 0) {
-				System.out.println("게시물이 없습니다.");
+				if (list.size() != 0) {
+					System.out.printf(" 현재 %d개의 글이 있습니다.\n", lastArticleid);
+					System.out.printf("	번호	|	제목	|	내용\n");
+					for (int i = 0; i < list.size(); i++) {
+						System.out.printf("	%d	|	%s	|	%s\n", list.get(i).getid(), list.get(i).getTitle(),
+								list.get(i).getbody());
+					}
+				}
 
+			} else if (command.equals("search")) {
+
+				System.out.println("열람을 원하는 게시물의 번호를 입력해주세요");
+				int num = sc.nextInt();
+
+				System.out.printf("	번호	|	제목	|	내용\n");
+				System.out.printf("	%d	|	%s	|	%s\n", list.get(num - 1).getid(), list.get(num - 1).getTitle(),
+						list.get(num - 1).getbody());
 			} else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어입니다.\n", command);
 			}
